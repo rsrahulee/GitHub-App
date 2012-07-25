c2231a3e83f5df08f6068f8f2726331bf792c066
rsrahulee <rahul@weboniselab.com>
2012-07-25 18:07:32 +0530

    handles the issues regarding loader and back button

--- src/com/github/GitHubAppActivity.java
+++ src/com/github/GitHubAppActivity.java
@@ -1,90 +1,88 @@
 package com.github;
 
-import com.github.helper.AppStatus;
-import com.github.helper.Constants;
-import com.github.repository.RepositoryActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.github.helper.AppStatus;
+import com.github.helper.Constants;
+
 public class GitHubAppActivity extends Activity {
-	
-	
+
 	private WebView webView;
 	AppStatus mAppStatus;
 	LoginInActivity context;
-	
-	
+
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.web_authorise);
- 
+
 		webView = (WebView) findViewById(R.id.webView);
 		webView.getSettings().setJavaScriptEnabled(true);
+
+		webView.setWebChromeClient(new WebChromeClient() {
+			public void onProgressChanged(WebView View, int progress) {
+				GitHubAppActivity.this.setProgress(progress * 1000);
+			}
+		});
 		webView.loadUrl("https://github.com/login/oauth/authorize?client_id=81e06b06df8444dfc400&scope=repo");
-		//https://github.com/login/oauth/authorize?client_id=81e06b06df8444dfc400
-		
+		// https://github.com/login/oauth/authorize?client_id=81e06b06df8444dfc400
+
 		webView.setWebViewClient(new AuthClient());
-			
+
 		webView.setOnTouchListener(new View.OnTouchListener() {
 
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				// TODO Auto-generated method stub
-				 WebView.HitTestResult hr = ((WebView)v).getHitTestResult();
-		         Log.i("******", "getExtra = "+ hr.getExtra() + "\t\t Type=" + hr.getType());
-		         
+				WebView.HitTestResult hr = ((WebView) v).getHitTestResult();
+				Log.i("******", "getExtra = " + hr.getExtra() + "\t\t Type=" + hr.getType());
+
 				return false;
 			}
-	    });
+		});
+		webView.setInitialScale(1);
+		Log.i("-----------", "-----------");
+	}
 
-    }
-	
-	
-	  private class AuthClient extends WebViewClient 
-	  {
-          @Override
-          public void onPageFinished(WebView v, String url) 
-          {
-              v.clearHistory();
-              Log.d("clear web view", "************");
-          }
-	  }
-
-	  
-	  @Override
-		public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-			if (keyCode == KeyEvent.KEYCODE_BACK) {
-				
-				Log.i("Backup Button", "Pressed");
-				
-				mAppStatus = AppStatus.getInstance(this);
-				
-				String name=getIntent().getExtras().getString("username");
-				
-				Constants.gitflag=true;
-				Intent i= new Intent(GitHubAppActivity.this, LoginInActivity.class); //RepositoryActivity
-				i.putExtra("username", name);
-				startActivity(i);				
-				finish();
-				
-			}
-			//super.onKeyDown(keyCode, event)
-			return true;
+	private class AuthClient extends WebViewClient {
+		@Override
+		public void onPageFinished(WebView v, String url) {
+			v.clearHistory();
+			Log.d("clear web view", "************");
 		}
+	}
 
-	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+			Log.i("Backup Button", "Pressed");
+
+			mAppStatus = AppStatus.getInstance(this);
+
+			String name = getIntent().getExtras().getString("username");
+
+			Constants.gitflag = true;
+			// Intent i = new Intent(this,
+			// GroupOrganisationRepositoryActivity.class);
+			Intent i = new Intent(GitHubAppActivity.this, LoginInActivity.class); // RepositoryActivity
+			i.putExtra("username", name);
+			startActivity(i);
+			finish();
+
+		}
+		// super.onKeyDown(keyCode, event)
+		return true;
+	}
+
 }
--- src/com/github/LoginInActivity.java
+++ src/com/github/LoginInActivity.java
@@ -19,51 +19,31 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginInActivity extends Activity {
-	
+
 	public ProgressDialog mProgressDialog;
 	AppStatus mAppStatus;
 	Handler mhandler;
-	
+
 	EditText txtUserName;
 	Button btnLogin;
-		
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
-		
-		mAppStatus = AppStatus.getInstance(this);
-		
-		txtUserName=(EditText) findViewById(R.id.editTextUserName);
-		btnLogin=(Button) findViewById(R.id.btnLoginIn);
-			
-		
-//		txtUserName.setOnFocusChangeListener(new OnFocusChangeListener() {
-//
-//			@Override
-//			public void onFocusChange(View v, boolean hasFocus) {
-//				// Your validation code goes here
-//				
-//				
-//				if ((txtUserName.getText().toString().trim()).equals("")) {
-//					Log.i("Empty Text", "@@@@@@@@");
-//				}
-//			}
-//		});
-		
-		onclickButton();
-	}
 
-	private void onclickButton(){
-		
+		mAppStatus = AppStatus.getInstance(this);
+
+		txtUserName = (EditText) findViewById(R.id.editTextUserName);
+		btnLogin = (Button) findViewById(R.id.btnLoginIn);
+
 		btnLogin.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
-				
+
 				String strUserName = txtUserName.getText().toString();
 				if (strUserName.equals("")) {
 					Log.i("Status Validation: ", "Please Enter username");
@@ -77,121 +57,116 @@ public class LoginInActivity extends Activity {
 			}
 		});
 	}
-	
 
-		public void validateUserName(final String userName) {
-			
-			if (mAppStatus.isOnline(LoginInActivity.this)) {
-				showDialog(0);
-				LoginTask mLoginTask = new LoginTask(LoginInActivity.this,userName);
-				mLoginTask.execute(userName);
-				
-			} else {
-				dismissDialog(0);
-				Log.v("SPLASH_SCREEN", "You are not online!!!!");
-				warningDialogBox("Please check you internet connection!!");
+	public void validateUserName(final String userName) {
+
+		if (mAppStatus.isOnline(LoginInActivity.this)) {
+			showDialog(0);
+			LoginTask mLoginTask = new LoginTask(LoginInActivity.this, userName);
+			mLoginTask.execute(userName);
+
+		} else {
+			dismissDialog(0);
+			Log.v("SPLASH_SCREEN", "You are not online!!!!");
+			warningDialogBox("Please check you internet connection!!");
+		}
+
+	}
+
+	// public void userAuthenticate(String strUserLoginName){
+	//
+	// Intent intent = new Intent(LoginInActivity.this,
+	// GroupRepositoryActivity.class);
+	// intent.putExtra("username",strUserLoginName);
+	// startActivity(intent);
+	// finish();
+	//
+	// }
+	//
+	//
+	// public void userNotAuthenticate(String strUserName){
+	// Intent intentWeb = new Intent(LoginInActivity.this,
+	// GitHubAppActivity.class);
+	// intentWeb.putExtra("username",strUserName);
+	// startActivity(intentWeb);
+	// finish();
+	// }
+
+	public void warningDialogBox(String warningText) {
+		// TODO Auto-generated method stub
+
+		// prepare the alert box
+		AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+
+		// set the message to display
+		alertbox.setMessage(warningText);
+
+		// add a neutral button to the alert box and assign a click listener
+		alertbox.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+
+			// click listener on the alert box
+			public void onClick(DialogInterface arg0, int arg1) {
+				// the button was clicked
 			}
-		
-		}
-		
-	
-//		public void userAuthenticate(String strUserLoginName){
-//
-//			Intent intent = new Intent(LoginInActivity.this, GroupRepositoryActivity.class); 
-//			intent.putExtra("username",strUserLoginName);
-//			startActivity(intent);
-//			finish();
-//			
-//		}
-//		
-//		
-//		public void userNotAuthenticate(String strUserName){
-//			Intent intentWeb = new Intent(LoginInActivity.this, GitHubAppActivity.class);
-//			intentWeb.putExtra("username",strUserName);
-//			startActivity(intentWeb);
-//			finish();
-//		}
-		
-		public void warningDialogBox(String warningText) {
-			// TODO Auto-generated method stub
+		});
 
-			// prepare the alert box
-			AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+		// show it
+		alertbox.show();
+	}
 
-			// set the message to display
-			alertbox.setMessage(warningText);
+	private void warningDialogBox() {
+		Log.i("Warning......Dialog", "ssssss");
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Are you sure you want to exit?").setCancelable(false)
+				.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
 
-			// add a neutral button to the alert box and assign a click listener
-			alertbox.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+						dialog.dismiss();
+						// moveTaskToBack(true);
+						finish();
+						return;
+					}
+				}).setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
 
-				// click listener on the alert box
-				public void onClick(DialogInterface arg0, int arg1) {
-					// the button was clicked
-				}
-			});
-
-			// show it
-			alertbox.show();
-		}
-		
-		private void warningDialogBox() {
-			Log.i("Warning......Dialog", "ssssss");
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage("Are you sure you want to exit?")
-					.setCancelable(false)
-					.setPositiveButton("Yes",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog, int id) {
-
-									dialog.dismiss();
-									//moveTaskToBack(true);
-									finish();
-									return;
-								}
-							})
-					.setNegativeButton("No", new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int id) {
-							dialog.cancel();
-						}
-					});
-			AlertDialog alert = builder.create();
-			alert.show();
-		}
-	
-		
-		/*-----------onCreateDialog method START ------ */
-		// Shows progress dialog box
-		@Override
-		protected Dialog onCreateDialog(int id) {
-			final ProgressDialog dialog = new ProgressDialog(this);
-			dialog.setTitle("Please Wait...");
-			dialog.setMessage("Loading.....");
-			dialog.setIndeterminate(true);
-			dialog.setCancelable(true);
-			dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-				@Override
-				public void onCancel(DialogInterface dialog) {
-					Log.i("GitHub", "user cancelling authentication");
-				}
-			});
-			mProgressDialog = dialog;
-			return dialog;
-		}
-
-		
-		/*-----------onCreateDialog method END ------ */
-
-		/*---------- Backup button captured ----------------- */
-		@Override
-		public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-			if (keyCode == KeyEvent.KEYCODE_BACK) {
-				Log.i("Backup Button", "Pressed");
-				warningDialogBox();
-				
+	/*-----------onCreateDialog method START ------ */
+	// Shows progress dialog box
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		final ProgressDialog dialog = new ProgressDialog(this);
+		dialog.setTitle("Please Wait...");
+		dialog.setMessage("Loading.....");
+		dialog.setIndeterminate(true);
+		dialog.setCancelable(true);
+		dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				Log.i("GitHub", "user cancelling authentication");
 			}
+		});
+		mProgressDialog = dialog;
+		return dialog;
+	}
 
-			return super.onKeyDown(keyCode, event);
-		}	
-		
+	/*-----------onCreateDialog method END ------ */
+
+	/*---------- Backup button captured ----------------- */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			Log.i("Backup Button", "Pressed");
+			warningDialogBox();
+
+		}
+
+		return super.onKeyDown(keyCode, event);
+	}
+
 }
